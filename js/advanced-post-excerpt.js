ca7c17f71cfc4add7d3969572abb5762a023750d
Dominik Stein <hello@steindominik.de>
2024-06-14 16:48:36 +0100

    Fix: 'core/edit-post' ) is deprecated
    Error Message inside of the Gutenberg Site Editor
    
    ////deprecated.min.js:2 dispatch( 'core/edit-post' ).removeEditorPanel is deprecated since version 6.5. Please use dispatch( 'core/editor').removeEditorPanel instead.

--- js/advanced-post-excerpt.js
+++ js/advanced-post-excerpt.js
@@ -6,5 +6,5 @@
  */
 
 // Remove the default "Post Excerpt" panel from the block editor.
-wp.data.dispatch( 'core/edit-post')
-	.removeEditorPanel( 'post-excerpt' );
+wp.data.dispatch('core/editor')
+    .removeEditorPanel('post-excerpt');
